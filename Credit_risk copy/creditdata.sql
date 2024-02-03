49c7c264d951e85b5f8bd806f2dd02e3b72b89fd
Toyakki <totto1001@icloud.com>
2024-02-03 18:27:55 -0500

    More rigour

---
+++ Credit_risk copy/creditdata.sql
@@ -0,0 +1,13 @@
+-- SQLite
+CREATE TABLE german_credit2(
+    Age INTEGER,
+    Sex TEXT,
+    Job INTEGER,
+    Housing TEXT,
+    Saving_accounts TEXT,
+    Checking_account TEXT,
+    Credit_amount INTEGER,
+    Duration INTEGER,
+    Purpose TEXT
+);
+SELECT * FROM german_credit;
